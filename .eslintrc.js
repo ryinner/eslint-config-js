882a0726dc02d6d0c4ccb8f1c6bac17ee9f30e6c
ryinner <vinograd18062002@gmail.com>
2023-06-27 16:00:58 +0500

    update eslint riles

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,44 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
+    'env': {
+        'browser': true,
+        'es2021': true,
+        'node': true
     },
-    "extends": "eslint:recommended",
-    "overrides": [
+    'extends': 'eslint:recommended',
+    'overrides': [
     ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+    'parserOptions': {
+        'ecmaVersion': 'latest',
+        'sourceType': 'module'
     },
-    "rules": {
-        "prefer-const": "error",
-        "prefer-exponentiation-operator": "error",
-        "prefer-object-spread": "warn",
-        "prefer-template": "error",
-        "for-direction": "error",
-        "no-duplicate-imports": "error",
-        "no-promise-executor-return": "error",
-        "no-self-compare": "error",
-        "no-template-curly-in-string": "error",
-        "arrow-body-style": ["error", "as-needed"],
-        "no-alert": "error",
-        "no-proto": "error",
-        "no-var": "error",
-        "no-tabs": "error",
-        "indent": ["error", 4, {"SwitchCase": 1}],
-        "array-bracket-spacing": "warn",
-        "no-multi-spaces": "warn",
-        "brace-style": ["error", "1tbs", {"allowSingleLine": true}],
-        "comma-spacing": "warn",
-        "func-call-spacing": ["error", "never"],
-        "implicit-arrow-linebreak": ["warn", "beside"],
-        "no-multiple-empty-lines": "error"
+    'rules': {
+        'max-params': ['error', 5],
+        'block-spacing': ['error', 'always'],
+        'prefer-const': 'error',
+        'prefer-exponentiation-operator': 'error',
+        'prefer-object-spread': 'warn',
+        'prefer-template': 'error',
+        'for-direction': 'error',
+        'no-duplicate-imports': 'error',
+        'no-promise-executor-return': 'error',
+        'no-self-compare': 'error',
+        'no-template-curly-in-string': 'error',
+        'arrow-spacing': ['error', { 'before': true, 'after': true }],
+        'arrow-body-style': ['error', 'as-needed'],
+        'no-alert': 'error',
+        'no-proto': 'error',
+        'no-var': 'error',
+        'no-tabs': 'error',
+        'no-trailing-spaces': 'warn',
+        'indent': ['error', 4, { 'SwitchCase': 1 }],
+        'quotes': ['error', 'single'],
+        'array-bracket-spacing': 'warn',
+        'brace-style': ['error', '1tbs', { 'allowSingleLine': true }],
+        'comma-spacing': 'warn',
+        'func-call-spacing': ['error', 'never'],
+        'implicit-arrow-linebreak': ['warn', 'beside'],
+        'no-multi-spaces': 'warn',
+        'no-multiple-empty-lines': 'error',
+        'rest-spread-spacing': ['warn', 'never']
     }
-}
+};
